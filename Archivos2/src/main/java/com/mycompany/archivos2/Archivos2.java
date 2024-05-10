1ed3760255c485ba2799bbb5082d62ef2e3b80ac
KarenMarcela <kmbayonam@ufpso.edu.co>
2024-05-10 07:14:28 -0500

    Refactorización del código

--- Archivos2/src/main/java/com/mycompany/archivos2/Archivos2.java
+++ Archivos2/src/main/java/com/mycompany/archivos2/Archivos2.java
@@ -10,16 +10,46 @@ import java.io.PrintWriter;
 
 public class Archivos2 {
 
-    public static void main(String[] args) throws IOException {
-        File file = new File("./archivo.txt");
-        file.createNewFile(); //TODO: Crear un mètodo para crear un archivo y maneajar la excepcion
-        
-        FileWriter writer = new FileWriter(file); //escribir en el archivo 
-        PrintWriter pw = new PrintWriter(writer); //Imprimir sobre el archivo creado
-        
-        for(int i=0; i<10 ; i++){
-            pw.println("Lìnea: "+(i+1));
+    public static void main(String[] args) {
+        try {
+            createFile("./archivo.txt");
+        } catch (IOException e) {
+            System.out.println("Error al crear el archivo: " + e.getMessage());
         }
-        writer.close();
     }
+
+    public static void createFile(String filePath) throws IOException { 
+        File file = new File(filePath);
+        if (file.createNewFile()) {
+            System.out.println("Archivo creado: " + file.getName());
+            writerAndFile(file);
+        } else {
+            System.out.println("El archivo ya existe.");
+        }
+    }
+
+    public static void writerAndFile(File file) throws IOException { //Escribir y Imprimir en el archivo
+        try (PrintWriter pw = new PrintWriter(new FileWriter(file))) {
+            for (int i = 0; i < 10; i++) {
+                pw.println("Línea: " + (i + 1));
+            }
+            System.out.println("Contenido agregado al archivo.");
+        }
+        
+
+    }
+//        
+//        File file = new File("./archivo.txt");
+//        file.createNewFile(); //TODO: Crear un mètodo para crear un archivo y maneajar la excepcion
+//        
+//        FileWriter writer = new FileWriter(file); //escribir en el archivo 
+//        PrintWriter pw = new PrintWriter(writer); //Imprimir sobre el archivo creado
+//        
+//        for(int i=0; i<10 ; i++){  
+//            pw.println("Lìnea: "+(i+1));
+//        }
+//        writer.close();
+//    }
+//
+
 }
